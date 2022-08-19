797d62d6bc586597d116bc3fed08b225a0d78152
davidly <david26ly@gmail.com>
2022-08-19 00:35:48 -0400

    formatted code to be more readable

--- src/components/Input.js
+++ src/components/Input.js
@@ -6,7 +6,10 @@ export default function Input(props) {
       type='text'
       value={props.input}
       onChange={props.handleChange}
-      className={`block w-24 mx-auto pb-2 bg-transparent border-b-2 ${props.isLight ? "border-b-slate-800" : "border-b-slate-200"} outline-none text-center text-6xl ${props.isLight ? "text-slate-800" : "text-slate-200"}`}
+      className={`block w-24 mx-auto pb-2 bg-transparent border-b-2 
+      ${props.isLight ? "border-b-slate-800" : "border-b-slate-200"} 
+      outline-none text-center text-6xl 
+      ${props.isLight ? "text-slate-800" : "text-slate-200"}`}
     />
   )
 }
